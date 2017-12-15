23d9d59e34e7bf5aec511b910075ce5414094a3d
SButko <silvija.butko@gmail.com>
2017-12-15 10:03:54 +0200

    [REF] float2words: joining with sfx
    Removing unnecessary helper method.
    Fixing joining numbers with suffixes.
    Changing method sequence by dependency.

--- float2words/float2words.py
+++ float2words/float2words.py
@@ -4,34 +4,6 @@ from num2words import num2words
 from decimal import Decimal, ROUND_HALF_UP
 
 
-def float2words(number, lang, sfx1='', sfx2='', connector=', ',
-                precision=2):
-    """Convert float to words by given optional parameters.
-
-    Args:
-        number (float): float number.
-        lang (str): language code (e.g. 'en_US', 'ru_RU', etc.).
-        sfx1 (str): suffix for whole part of float number
-            (e.g. 'USD', '$', 'kg', etc.).
-        sfx2 (str): suffix for decimal part of float number.
-            (e.g. 'ct.', 'g', etc.)
-        connector (str): connector will be used between whole and
-            decimal parts expressed in words.
-        precision (int): number of digits after decimal point.
-
-    Returns:
-        number_in_words (str): float number in words.
-
-    """
-    whole_part, decimal_part = _split_float(number, precision)
-    number_in_words = ''.join([
-        _convert_and_combine(whole_part, lang, sfx1),
-        connector,
-        _convert_and_combine(decimal_part, lang, sfx2)
-    ])
-    return number_in_words
-
-
 def _split_float(number, precision):
     """Split float number into whole and decimal parts.
 
@@ -55,22 +27,28 @@ def _split_float(number, precision):
     return whole_part, decimal_part
 
 
-def _convert_and_combine(number, lang, suffix):
-    """Convert number (integer) to words and combine with suffix.
+def float2words(number, lang, sfx1='', sfx2='', connector=', ', precision=2):
+    """Convert float to words by given optional parameters.
 
     Args:
-        number (integer): integer number.
+        number (float): float number.
         lang (str): language code (e.g. 'en_US', 'ru_RU', etc.).
-        suffix (str): suffix.
+        sfx1 (str): suffix for whole part of float number
+            (e.g. 'USD', '$', 'kg', etc.).
+        sfx2 (str): suffix for decimal part of float number.
+            (e.g. 'ct.', 'g', etc.)
+        connector (str): connector will be used between whole and
+            decimal parts expressed in words.
+        precision (int): number of digits after decimal point.
 
     Returns:
-        (str): combined number in words and suffix.
+        number_in_words (str): float number in words.
 
     """
-    # add whitespace before suffix if suffix is passed.
-    return ' '.join([
-        item for item in [
-            num2words(number, lang=lang),
-            suffix
-        ] if item
+    whole_part, decimal_part = _split_float(number, precision)
+    number_in_words = ''.join([
+        ' '.join(filter(None, [num2words(whole_part, lang=lang), sfx1])),
+        connector,
+        ' '.join(filter(None, [num2words(decimal_part, lang=lang), sfx2]))
     ])
+    return number_in_words
