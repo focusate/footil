09d8c6b3a35f671cf908ffc9f4668357e510d8b7
Andrius Laukavičius <andrius@focusate.eu>
2019-03-20 15:32:31 +0200

    [REF] setup.py: updated setup
    Added MANIFEST.in to include README.rst file which is outside footil
    package and is not added by default when running sdist.
    
    Also added LICENSE file.

--- README.rst
+++ README.rst
@@ -6,6 +6,11 @@ Has these modules:
 * decor - helper decorators.
 * log - helpers with stdout, stderr management.
 
+Source code in:
+
+* `github <https://github.com/focusate/footil/>`_.
+* `pypi <https://pypi.org/project/footil/>`_.
+
 Contributors
 ============
 
--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from distutils.core import setup
 
 setup(
-    name='Footil',
-    version='0.2.0',
+    name='footil',
+    version='0.4.0',
     packages=['footil'],
     license='LGPLv3',
+    url='https://github.com/focusate/footil',
+    description="Various Python helpers for other projects",
     long_description=open('README.rst').read(),
-    install_requires=['yattag']
+    install_requires=['yattag'],
+    maintainer='Andrius Laukavičius',
+    maintainer_email='dev@focusate.eu',
+    python_requires='>=3',
+    classifiers=[
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+    ],
 )
