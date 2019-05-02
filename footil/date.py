a02c66d6422e15c092a0deba4ab4fb306b3e71c3
SButko <silvija@focusate.eu>
2019-05-02 13:29:42 +0300

    [IMP] date.py: datetime.date obj support
    Implementing separate methods to get dates from date string to date
    string and from date object to date object.
    
    [BRANCH] feature/date-manipulation-sbu

--- footil/date.py
+++ footil/date.py
@@ -8,24 +8,72 @@ DATETIME_FMT = '%Y-%m-%d %H:%M:%S'
 
 
 def _modify_date(
-    dt: str,
-    fmt: str,
+    dt: datetime.date,
     modify_fun: callable,
-    new_fmt: Optional[str] = False,
     args: Optional[tuple] = None,
-        kwargs: Optional[dict] = None) -> str:
+        kwargs: Optional[dict] = None) -> datetime.date:
     if not args:
         args = []
     if not kwargs:
         kwargs = {}
-    if not new_fmt:
-        new_fmt = fmt
-    dt = datetime.datetime.strptime(dt, fmt)
-    modified_dt = modify_fun(dt, *args, **kwargs)
-    return modified_dt.strftime(new_fmt)
+    return modify_fun(dt, *args, **kwargs)
+
+
+def to_date(
+    dt: str,
+        fmt: Optional[str] = DATE_FMT) -> datetime.date:
+    """Convert date string of custom format to datetime.date object."""
+    return datetime.datetime.strptime(dt, fmt).date()
+
+
+def to_string(
+    dt: datetime.date,
+        fmt: Optional[str] = DATE_FMT) -> str:
+    """Convert datetime.date object to custom format date string."""
+    return dt.strftime(fmt)
 
 
 def get_first_day_date(
+    dt: datetime.date,
+        months: int = 0) -> datetime.date:
+    """Return date by replacing day to be first day of the month.
+
+    Args:
+        dt (datetime.date): datetime.date object to modify.
+        months (int): x month
+            (0 - this month, 1 - next month, -1 - last month, ...)
+
+    Returns:
+        datetime.date: modified date as datetime.date object with day
+            being first of the month.
+
+    """
+    return _modify_date(
+        dt,
+        lambda dt: dt + relativedelta(day=1, months=months))
+
+
+def get_last_day_date(
+    dt: datetime.date,
+        months: int = 0) -> datetime.date:
+    """Return date by replacing day to be last day of the month.
+
+    Args:
+        dt (datetime.date): datetime.date object to modify.
+        months (int): x month
+            (0 - this month, 1 - next month, -1 - last month, ...)
+
+    Returns:
+        datetime.date: modified date as datetime.date object with day
+            being last of the month.
+
+    """
+    return _modify_date(
+        dt,
+        lambda dt: dt + relativedelta(day=1, days=-1, months=(months + 1)))
+
+
+def get_first_day_date_str(
     dt: str,
     fmt: Optional[str] = DATE_FMT,
     new_fmt: Optional[str] = False,
@@ -33,7 +81,7 @@ def get_first_day_date(
     """Return date by replacing day to be first day of the month.
 
     Args:
-        dt (str): date string to modify
+        dt (str): date as string to modify.
         fmt (str): format to convert from (default: {DATE_FMT})
         new_fmt (str): format to convert to. If not set, will use
             fmt variable format. (default: {False})
@@ -44,22 +92,22 @@ def get_first_day_date(
         str: modified date with day being first of the month.
 
     """
-    return _modify_date(
-        dt,
-        fmt,
-        lambda dt: dt + relativedelta(day=1, months=months),
-        new_fmt=new_fmt)
+    if not new_fmt:
+        new_fmt = fmt
+    date_fmt = to_date(dt, fmt=fmt)
+    modified_dt = get_first_day_date(date_fmt, months=months)
+    return to_string(modified_dt, fmt=new_fmt)
 
 
-def get_last_day_date(
-    dt: str,
+def get_last_day_date_str(
+    dt: datetime.date,
     fmt: Optional[str] = DATE_FMT,
     new_fmt: Optional[str] = False,
         months: int = 0) -> str:
     """Return date by replacing day to be last day of the month.
 
     Args:
-        dt (str): date string to modify
+        dt (str): date as string to modify.
         fmt (str): format to convert from (default: {DATE_FMT})
         new_fmt (str): format to convert to. If not set, will use
             fmt variable format. (default: {False})
@@ -70,8 +118,8 @@ def get_last_day_date(
         str: modified date with day being last of the month.
 
     """
-    return _modify_date(
-        dt,
-        fmt,
-        lambda dt: dt + relativedelta(day=1, days=-1, months=(months + 1)),
-        new_fmt=new_fmt)
+    if not new_fmt:
+        new_fmt = fmt
+    date_fmt = to_date(dt, fmt=fmt)
+    modified_dt = get_last_day_date(date_fmt, months=months)
+    return to_string(modified_dt, fmt=new_fmt)
