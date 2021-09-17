98a479e73f2d07e0a9a4a980f3bc374f5d1e6e41
Andrius Laukavičius <andrius@focusate.eu>
2021-09-17 10:30:40 +0300

    [ADD] footil/xcollections: to_intervals
    [BRANCH] feature/seq-to-intervals-ala

--- footil/tests/test_xcollections.py
+++ footil/tests/test_xcollections.py
@@ -1,4 +1,4 @@
-from footil import xcollections
+from footil.xcollections import batch, to_intervals
 
 from . import common
 
@@ -10,16 +10,82 @@ class TestCollections(common.TestFootilCommon):
 
     def test_01_batch(self):
         """Check batches: sequence length < batch size."""
-        self.assertEqual(list(xcollections.batch(SEQUENCE_1, 6)), [SEQUENCE_1])
+        self.assertEqual(list(batch(SEQUENCE_1, 6)), [SEQUENCE_1])
 
     def test_02_batch(self):
         """Check batches: sequence length > batch size (2 batches)."""
-        self.assertEqual(
-            list(xcollections.batch(SEQUENCE_1, 3)), [[1, 2, 3], [4, 5]]
-        )
+        self.assertEqual(list(batch(SEQUENCE_1, 3)), [[1, 2, 3], [4, 5]])
 
     def test_03_batch(self):
         """Check batches: sequence length > batch size (5 batches)."""
+        self.assertEqual(list(batch(SEQUENCE_1, 1)), [[1], [2], [3], [4], [5]])
+
+    def test_04_to_intervals(self):
+        """Interval unsorted, with dupes [2, 1, 2, 4]."""
         self.assertEqual(
-            list(xcollections.batch(SEQUENCE_1, 1)), [[1], [2], [3], [4], [5]]
+            to_intervals([2, 1, 2, 4], no_dupes=False, sort=False),
+            [(2, 1), (1, 2), (2, 4)],
+        )
+
+    def test_05_to_intervals(self):
+        """Interval unsorted, without dupes [2, 1, 2, 4]."""
+        self.assertEqual(
+            to_intervals([2, 1, 2, 4], no_dupes=True, sort=False),
+            [(2, 1), (1, 4)],
+        )
+
+    def test_06_to_intervals(self):
+        """Interval sorted, with dupes [2, 1, 2, 4]."""
+        self.assertEqual(
+            to_intervals([2, 1, 2, 4], no_dupes=False, sort=True),
+            [(1, 2), (2, 2), (2, 4)],
+        )
+
+    def test_07_to_intervals(self):
+        """Interval sorted, without dupes [2, 1, 2, 4]."""
+        self.assertEqual(
+            to_intervals([2, 1, 2, 4], no_dupes=True, sort=True),
+            [(1, 2), (2, 4)],
+        )
+
+    def test_08_to_intervals(self):
+        """Interval incorrect sequence.
+
+        Case 1: no items in sequence.
+        Case 2: one item in sequence only.
+        """
+        # Case 1.
+        self.assertEqual(to_intervals([]), [])
+        # Case 2.
+        self.assertEqual(to_intervals([2]), [])
+
+    def test_09_to_intervals(self):
+        """Interval sorted with key ['1B', '2A', '1C', '1C'].
+
+        Case 1: reverse=False
+        Case 2: reverse=True
+        """
+        # Case 1.
+        self.assertEqual(
+            to_intervals(
+                ['1B', '2A', '1C'],
+                no_dupes=True,
+                sort=True,
+                reverse=False,
+                # Sort by letters.
+                key=lambda i: i[1]
+            ),
+            [('2A', '1B'), ('1B', '1C')],
+        )
+        # Case 2.
+        self.assertEqual(
+            to_intervals(
+                ['1B', '2A', '1C'],
+                no_dupes=True,
+                sort=True,
+                reverse=True,
+                # Sort by letters.
+                key=lambda i: i[1]
+            ),
+            [('1C', '1B'), ('1B', '2A')],
         )
